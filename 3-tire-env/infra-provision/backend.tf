e78448521d331f79532d9292ab34057938247f04
Raghavendra17N <raghava01995@gamil.com>
2023-03-26 20:00:35 +0530

    metadata-key

--- 3-tire-env/infra-provision/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "charged-muse-376504-iac-tf-state"
-    prefix = "env/three-tire-env"
-  }
-}
--- 3-tire-env/infra-provision/modules/VPC/vpc.tf
+++
@@ -1,9 +0,0 @@
-resource "google_compute_network" "vpc" {
-  auto_create_subnetworks = false
-  delete_default_routes_on_create = var.delete_default_routes_on_create
-  description = var.description
-  name = var.name
-  project = var.project
-  routing_mode = var.routing_mode
-
-}
